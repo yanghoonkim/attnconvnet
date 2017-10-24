4683b5709a9f52f3b818909b7185b1f18e737ff6
yanghoonkim <ad26kt@gmail.com>
2017-10-24 13:56:21 +0900

    initial commit for attention = relation

--- model.py
+++ model.py
@@ -6,6 +6,39 @@ import tensorflow as tf
 
 import common_attention as ca
 import common_layers as cl
+def relation(inputs, params):
+    depth = inputs.get_shape().as_list()[-1]
+
+    a = tf.identity(inputs)
+    b = tf.identity(inputs)
+    
+    # [filter_width, in_channel, out_channel]
+    w1 = tf.get_variable('w1', [1, depth, params['relation_size']])
+    w2 = tf.get_variable('w2', [1, depth, params['relation_size']])
+
+    a = tf.nn.conv1d(a, w1, 1, 'VALID')
+    b = tf.nn.conv1d(b, w2, 1, 'VALID')
+    length = a.get_shape().as_list()[1]
+    depth = a.get_shape().as_list()[-1]
+
+    # [batch, length*length, depth]
+    a = tf.tile(a, [1, length, 1])
+    #[batch, length, length, depth]
+    a = tf.reshape(a, [-1, length, length, depth])
+    
+    #[batch, length, depth * length]
+    b = tf.tile(b, [1, 1, length])
+    #[batch, length, length, depth]
+    b = tf.reshape(b, [-1, length, length, depth])
+
+    a_b = tf.tanh(a + b)
+    a_b = tf.reduce_sum(a_b, [1,2])
+
+    w_f = tf.get_variable('w_f', [params['relation_size'], params['label_size']])
+    b_f = tf.get_variable('b_f', [params['label_size']])
+    return tf.tanh(tf.matmul(a_b, w_f) + b_f)
+    
+
 
 def attn_net(features, labels, mode, params):
     hidden_size = params['hidden_size']
@@ -94,21 +127,22 @@ def attn_net(features, labels, mode, params):
         x = residual_fn(x, transformer_ffn_layer(x, params))
         
     # convolution 
-    convout = conv_op(x, params)
+    #convout = conv_op(x, params)
     
-    channel_out = params['kernel'][-1]
+    #channel_out = params['kernel'][-1]
     # width_out = (feature_length - windows_size)/stride + 1
-    width_out = (x.get_shape().as_list()[1] - params['kernel'][0])/params['stride'] + 1
+    #width_out = (x.get_shape().as_list()[1] - params['kernel'][0])/params['stride'] + 1
 
 
     # pooling over time
-    convout = tf.reshape(convout, [-1, width_out, 1, channel_out])
-    pooling = tf.nn.max_pool(convout, [1, width_out, 1, 1], [1, 1, 1, 1], 'VALID')
-    pooling = tf.reshape(pooling, [-1, channel_out])
+    #convout = tf.reshape(convout, [-1, width_out, 1, channel_out])
+    #pooling = tf.nn.max_pool(convout, [1, width_out, 1, 1], [1, 1, 1, 1], 'VALID')
+    #pooling = tf.reshape(pooling, [-1, channel_out])
     
-    ffn_out = ffn_op(pooling, params)
+    #ffn_out = ffn_op(pooling, params)
+    relation_out = relation(x, params)
     
-    softmax_out = tf.nn.softmax(ffn_out)
+    softmax_out = tf.nn.softmax(relation_out)
     
     predictions = tf.argmax(softmax_out, axis = -1)
     
@@ -121,7 +155,7 @@ def attn_net(features, labels, mode, params):
                 predictions={"sentiment": predictions})
     labels = tf.cast(labels, tf.int32)
     labels = tf.one_hot(labels, params['label_size']) 
-    loss = tf.losses.softmax_cross_entropy(onehot_labels = labels, logits = ffn_out)
+    loss = tf.losses.softmax_cross_entropy(onehot_labels = labels, logits = relation_out)
     
     # accuracy as evaluation metric
     eval_metric_ops = { 
--- params.py
+++ params.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-hidden = 50
+hidden =30
 def basic_params():
     '''A set of basic hyperparameters'''
     return tf.contrib.training.HParams(
@@ -11,18 +11,21 @@ def basic_params():
         bucket_sizes = [10, 20, 30, 40, 50],
         
         # attention network parameters
-        num_layers = 1,
-        num_heads =5,
+        num_layers = 3,
+        num_heads =1,
         attn_dropout = 0.1,
         residual_dropout = 0.1,
         relu_dropout = 0.0,
-        filter_size = 256,
+        filter_size = 100,
         
         # convolution parameters
         kernel = [5, hidden, 20], # kernel shape for tf.nn.conv1d, [filter_width, in_channels, out_channels]
         stride = 5,
         conv_pad = 'VALID', # 'VALID' or 'SAME'
         
+        # relation parameters
+        relation_size = 30,
+
         # fully connected network parameters
         ffn_size = None,
         
