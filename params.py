a8f88990e6458ddf06f6662b150cbc66bcf53526
yanghoonkim <ad26kt@gmail.com>
2017-10-30 14:36:40 +0900

    add test_params, set evaluation delay, change data

--- params.py
+++ params.py
@@ -4,10 +4,10 @@ def basic_params():
     '''A set of basic hyperparameters'''
     return tf.contrib.training.HParams(
         dtype = tf.float32,
-        voca_size = 8413,
+        voca_size = 8414,
         label_size = 6, 
-        hidden_size = hidden,
-        value_depth = hidden,
+        hidden_size = 50,
+        value_depth = 50,
         bucket_sizes = [10, 20, 30, 40, 50],
         
         # attention network parameters
@@ -19,7 +19,7 @@ def basic_params():
         filter_size = 256,
         
         # convolution parameters
-        kernel = [5, hidden, 20], # kernel shape for tf.nn.conv1d, [filter_width, in_channels, out_channels]
+        kernel = [5, 50, 30], # kernel shape for tf.nn.conv1d, [filter_width, in_channels, out_channels]
         stride = 5,
         conv_pad = 'VALID', # 'VALID' or 'SAME'
         
@@ -28,6 +28,42 @@ def basic_params():
         
         # learning parameters
         batch_size = 512,
+        learning_rate = 0.1
+        
+    )
+
+# trec: up to 0.876, maxlength20, voca 8414
+def basic_1():
+    return hparams
+hidden = 50
+def test_params():
+    '''A set of basic hyperparameters'''
+    return tf.contrib.training.HParams(
+        dtype = tf.float32,
+        voca_size = 8680,
+        label_size = 6, 
+        hidden_size = hidden,
+        value_depth = hidden,
+        bucket_sizes = [10, 20, 30, 40, 50],
+        
+        # attention network parameters
+        num_layers = 1,
+        num_heads =10,
+        attn_dropout = 0.1,
+        residual_dropout = 0.1,
+        relu_dropout = 0.0,
+        filter_size = 150,
+        
+        # convolution parameters
+        kernel = [10, hidden, 30], # kernel shape for tf.nn.conv1d, [filter_width, in_channels, out_channels]
+        stride = 1,
+        conv_pad = 'VALID', # 'VALID' or 'SAME'
+        
+        # fully connected network parameters
+        ffn_size = None,
+        
+        # learning parameters
+        batch_size = 20,
         learning_rate = 0.1
         
     )
