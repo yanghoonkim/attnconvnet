6dd408b5ddf9c30b6bcd596b7b2abdf50ad6f343
yanghoonkim <ad26kt@gmail.com>
2017-12-27 15:28:54 +0900

    Change to one model with multi data setting
    - model.py
        add accuracy4multilabel function
        switch between random embedding and pre-trained embedding
        auto conversion from embedding size to hidden size
        switch between single label setting and multi label setting
        add eval_metric_ops
    - params.py
        integrate params with multiple data
        add new variables : embedding, embedding_trainable, multi_label
    - run_attention.sh
        add shell function for each dataset to switch between them

--- params.py
+++ params.py
@@ -1,69 +1,84 @@
 import tensorflow as tf
-hidden = 50
 def basic_params():
     '''A set of basic hyperparameters'''
     return tf.contrib.training.HParams(
         dtype = tf.float32,
-        voca_size = 8414,
-        label_size = 6, 
+        voca_size = 8680,
+        embedding = None,
+        embedding_trainable = True,
+        label_size = 6,
+        multi_label = 1, # if >1, multi-label setting
         hidden_size = 50,
         value_depth = 50,
         bucket_sizes = [10, 20, 30, 40, 50],
         
         # attention network parameters
         num_layers = 1,
-        num_heads =5,
+        num_heads = 10,
         attn_dropout = 0.1,
         residual_dropout = 0.1,
-        relu_dropout = 0.0,
-        filter_size = 256,
+        relu_dropout = 0.1,
+        filter_size = 128,
         
         # convolution parameters
-        kernel = [5, 50, 30], # kernel shape for tf.nn.conv1d, [filter_width, in_channels, out_channels]
-        stride = 5,
+        kernel = [10, 50, 30], # kernel shape for tf.nn.conv1d, [filter_width, in_channels, out_channels]
+        stride = 1,
         conv_pad = 'VALID', # 'VALID' or 'SAME'
         
         # fully connected network parameters
         ffn_size = None,
         
         # learning parameters
-        batch_size = 512,
-        learning_rate = 0.1
-        
-    )
-
-# trec: up to 0.876, maxlength20, voca 8414
-def basic_1():
-    return hparams
-hidden = 20
-def test_params():
-    '''A set of basic hyperparameters'''
-    return tf.contrib.training.HParams(
-        dtype = tf.float32,
-        voca_size = 13376,
-        label_size = 1, 
-        hidden_size = hidden,
-        value_depth = hidden,
-        bucket_sizes = [10, 20, 30, 40, 50],
-        
-        # attention network parameters
-        num_layers = 3,
-        num_heads =2,
-        attn_dropout = 0.1,
-        residual_dropout = 0.1,
-        relu_dropout = 0.1,
-        filter_size = 64,
-        
-        # convolution parameters
-        kernel = [10, hidden, 30], # kernel shape for tf.nn.conv1d, [filter_width, in_channels, out_channels]
-        stride = 1,
-        conv_pad = 'VALID', # 'VALID' or 'SAME'
-        
-        # fully connected network parameters
-        ffn_size = None, # list of scalars
-        
-        # learning parameters
         batch_size = 20,
         learning_rate = 0.02
         
     )
+
+def trec_params():
+    hparams = basic_params()
+    hparams.embedding = 'data/trec/processed/glove840b_trec_vocab300.npy'
+    return hparams
+
+def sst5_params():
+    hparams = basic_params()
+    hparams.voca_size = 16560
+    hparams.embedding = 'data/sst/processed_git/glove840b_sst5_vocab300.npy'
+    hparams.embedding_trainable = True
+    hparams.label_size = 5
+    hparams.filter_size = 100
+    hparams.batch_size = 150
+    hparams.learning_rate = 0.03
+    return hparams
+
+def sem4_params():
+    hparams = basic_params()
+    hparams.voca_size = 4674
+    hparams.embedding = 'data/semeval/processed/glove840b_semeval1_4_vocab300.npy'
+    hparams.embedding_trainable = False
+    hparams.label_size = 7
+    hparams.hidden_size = 20
+    hparams.value_depth = 20
+    hparams.num_layers = 3
+    hparams.num_heads = 2
+    hparams.filter_size = 64
+    hparams.kernel = [10, 20, 30]
+    hparams.batch_size = 20
+    hparams.learning_rate = 0.02
+    return hparams
+
+def sem5_params():
+    hparams = basic_params()
+    hparams.voca_size = 13376
+    hparams.embedding = 'data/semeval/processed/glove840b_semeval1_5_vocab300.npy'
+    hparams.embedding_trainable = False
+    hparams.label_size = 1
+    hparams.multi_label = 11
+    hparams.hidden_size = 20
+    hparams.value_depth = 20
+    hparams.num_layers = 3
+    hparams.num_heads = 2
+    hparams.filter_size = 64
+    hparams.kernel = [10, 20, 30]
+    hparams.batch_size = 20
+    hparams.learning_rate = 0.02
+    return hparams
--- run_attention.sh
+++ run_attention.sh
@@ -7,15 +7,44 @@
 #!/bin/bash
 MODE='train'
 
-TRAIN_INPUT='data/semeval/processed/ec_train.npy'
-TRAIN_TARGET='data/semeval/processed/ec_train_label.npy'
-TEST_INPUT='data/semeval/processed/ec_test.npy'
-TEST_TARGET='data/semeval/processed/ec_test_label.npy'
+trec(){
+	TRAIN_INPUT='data/trec/processed/trec_train.npy'
+	TRAIN_TARGET='data/trec/processed/trec_train_label.npy'
+	TEST_INPUT='data/trec/processed/trec_test.npy'
+	TEST_TARGET='data/trec/processed/trec_test_label.npy'
+	PARAMS=trec_params
+}
+
+sst5(){
+	TRAIN_INPUT='data/sst/processed_git/sst5_train.npy'
+	TRAIN_TARGET='data/sst/processed_git/sst5_train_label.npy'
+	TEST_INPUT='data/sst/processed_git/sst5_test.npy'
+	TEST_TARGET='data/sst/processed_git/sst5_test_label.npy'
+	PARAMS=sst5_params
+}
+
+sem4(){
+	TRAIN_INPUT='data/semeval/processed/voc_train.npy'
+	TRAIN_TARGET='data/semeval/processed/voc_train_label.npy'
+	TEST_INPUT='data/semeval/processed/voc_test.npy'
+	TEST_TARGET='data/semeval/processed/voc_test_label.npy'
+	PARAMS=sem4_params
+}
+
+sem5(){
+	TRAIN_INPUT='data/semeval/processed/ec_train.npy'
+	TRAIN_TARGET='data/semeval/processed/ec_train_label.npy'
+	TEST_INPUT='data/semeval/processed/ec_test.npy'
+	TEST_TARGET='data/semeval/processed/ec_test_label.npy'
+	PARAMS=sem5_params
+}
+
+
+# Pass the first argument as the name of dataset
+$1
 
 TRAIN_STEPS=200000
 NUM_EPOCHS=None
-
-PARAMS=test_params
 MODEL_DIR=~/work/attentionnet/store_model/$PARAMS
 
 python main.py \
